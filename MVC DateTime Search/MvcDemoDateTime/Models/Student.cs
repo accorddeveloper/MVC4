ec2559ab885dbc89f3f73b84a364b6ab00ec604b
mdsahid <mdsahidh97@gmail.com>
2017-04-06 13:08:14 +0600

    DateTime Search File Added

---
+++ MVC DateTime Search/MvcDemoDateTime/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace MvcDemoDateTime.Models
+{
+    public class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [DisplayFormat(DataFormatString = "{0:MMM dd, yyyy}")]
+        public DateTime AdmissionDate { get; set; }
+    }
+}
